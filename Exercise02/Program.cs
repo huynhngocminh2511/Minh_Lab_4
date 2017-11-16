d99b0fccf4919d356bbfa1199d08c147caf4ca8f
U-MINH\huynh <huynhngocminh2511@gmail.com>
2017-11-16 20:18:27 +0700

    Add Exercise02

---
+++ Exercise02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = "heLlO";
+            Console.WriteLine(str.CaptionFirst());
+            Console.ReadKey();
+        }
+    }
+
+    public static class StringExtension
+    {
+        public static string CaptionFirst(this string str)
+        {
+            var firststr = str[0].ToString();
+            firststr = firststr.ToUpper();
+            var secondnstr = str.Substring(1);
+            secondnstr = secondnstr.ToLower();
+            return firststr + secondnstr;
+        }
+    }
+}
