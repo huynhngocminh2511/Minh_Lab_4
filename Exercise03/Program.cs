293196a671b770e6c2cd8160ec339e9ba99abf95
U-MINH\huynh <huynhngocminh2511@gmail.com>
2017-11-16 20:25:16 +0700

    Add Exercise03

---
+++ Exercise03/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var q = 5;
+            Console.WriteLine(q.IsPrime());
+            q = 7;
+            Console.WriteLine(q.IsPrime());
+            q = 9;
+            Console.WriteLine(q.IsPrime());
+            q = 10;
+            Console.WriteLine(q.IsPrime());
+            Console.ReadKey();
+        }
+    }
+
+    public static class StringExtension
+    {
+        public static bool IsPrime(this int q)
+        {
+            for (int i = 2; i <= (int)Math.Sqrt(q); i++)
+            {
+                if (q % i == 0) 
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
