2ce5bfe8e542899936d61ff3aa50a54ea942b20b
U-MINH\huynh <huynhngocminh2511@gmail.com>
2017-11-17 10:09:34 +0700

    Add Exercise05

---
+++ Exercise05/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var qu = new Queue<int>();
+            qu.Add(1);
+            qu.Add(2);
+            qu.Add(3);
+            while (!qu.Isempty())
+            {
+                Console.WriteLine(qu.Front());
+                qu.Pop();
+            }
+            var qu2 = new Queue<string>();
+            qu2.Add("a");
+            qu2.Add("b");
+            qu2.Add("c");
+            while (!qu2.Isempty())
+            {
+                Console.WriteLine(qu2.Front());
+                qu2.Pop();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exercise05/Queue.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise05
+{
+    class Queue<T>
+    {
+        public List<T> list { set; get; }
+
+        public Queue()
+        {
+            list = new List<T>();
+        }
+
+        public void Pop()
+        {
+            try
+            {
+                list.RemoveAt(0);
+            } catch (Exception e)
+            {
+                Console.WriteLine("Queue is empty");
+            }
+        }
+
+        public T Front()
+        {
+            try
+            {
+                return list.First();
+            } catch (Exception e)
+            {
+                Console.WriteLine("Queue is empty");
+            }
+            return default(T);
+        }
+
+        public void Add(T t)
+        {
+            list.Add(t);
+        }
+
+        public bool Isempty()
+        {
+            return (list.Count == 0);
+        }
+    }
+}
