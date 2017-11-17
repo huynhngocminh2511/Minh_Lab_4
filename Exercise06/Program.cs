3dafa9046143810ad7e7eb2f12aeb65b4a6b472f
U-MINH\huynh <huynhngocminh2511@gmail.com>
2017-11-17 10:34:03 +0700

    Add Exercise06

---
+++ Exercise06/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var st = new Stack<int>();
+            st.Add(1);
+            st.Add(2);
+            st.Add(3);
+            while (!st.Isempty())
+            {
+                Console.WriteLine(st.Top());
+                st.Pop();
+            }
+            var st2 = new Stack<string>();
+            st2.Add("a");
+            st2.Add("b");
+            st2.Add("c");
+            while (!st2.Isempty())
+            {
+                Console.WriteLine(st2.Top());
+                st2.Pop();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exercise06/Stack.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise06
+{
+    class Stack<T>
+    {
+        public List<T> list { set; get; }
+
+        public Stack()
+        {
+            list = new List<T>();
+        }
+
+        public void Pop()
+        {
+            try
+            {
+                list.RemoveAt(list.Count - 1);
+            } catch (Exception e)
+            {
+                Console.WriteLine("Stack is empty");
+            }
+        }
+
+        public T Top()
+        {
+            return list.LastOrDefault();
+        }
+
+        public void Add(T t)
+        {
+            list.Add(t);
+        }
+
+        public bool Isempty()
+        {
+            return (list.Count == 0);
+        }
+    }
+}
